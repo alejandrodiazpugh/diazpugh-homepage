1971284d20bbf77651c2f9528216944ed9d967cf
Alejandro Díaz <alejandrodiazpugh@gmail.com>
2022-11-20 11:54:00 -0600

    hero layot and button styling changes

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,6 +6,6 @@ type Props = {
 };
 
 export default function Button({ content, colorClasses }: Props) {
-	const className = `${colorClasses} h-20 w-48 md:h-28 md:w-72 rounded-3xl border-2 md:text-2xl transition-all font-sourceCode`;
+	const className = `${colorClasses} h-20 w-48 md:h-28 md:w-64 shadow-md tracking-widest rounded-3xl border-2 md:text-xl transition-all font-sourceCode`;
 	return <button className={className}>{content}</button>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,50 +17,57 @@ export default function Home() {
 			</Head>
 			<article
 				id="hero"
-				className="relative z-50 flex h-screen w-screen snap-y snap-mandatory flex-col items-center justify-around text-blue lg:flex-row"
+				className="relative z-50 grid h-screen w-screen snap-y snap-mandatory grid-cols-2 text-blue lg:flex-row"
 			>
-				<div className=" flex flex-col justify-center">
-					<motion.div
-						initial={{ opacity: 0, scale: 0.2 }}
-						animate={{ opacity: 0.2, scale: 1 }}
-						transition={{
-							duration: 1,
-							delay: 1,
-							type: 'spring',
-						}}
-						className="absolute -z-10 grid aspect-square w-[70%] place-self-center rounded-full border-[1px] border-orange bg-white opacity-20 md:w-[50%]"
-					>
-						<Image src={logo} alt={'Logo de diazpugh'} />
-					</motion.div>
+				<motion.div
+					initial={{ x: '-100%' }}
+					animate={{ x: 0 }}
+					transition={{
+						duration: 1,
+						delay: 1,
+					}}
+					className="col-span-1 col-start-1 flex flex-col justify-center"
+				>
 					<h1 className="capitalized mx-auto max-w-md snap-y snap-mandatory pt-28 text-center font-poppins text-5xl font-[500] md:max-w-2xl md:text-7xl">
 						{copy.title}
 					</h1>
-					<h2 className="mx-auto my-10 max-w-2xl text-center font-sourceCode text-2xl font-semibold md:text-4xl">
+					<h2 className="mx-auto my-10 max-w-2xl text-center font-sourceCode text-lg font-semibold md:text-2xl">
 						{copy.subtitle}
 					</h2>
 					<div className="mx-auto">
 						<Button
 							content={'comenzamos?'}
 							colorClasses={
-								'border-cyan bg-white text-blue hover:border-blue hover:bg-blue hover:text-white'
+								'border-pink bg-pink text-white hover:border-pink hover:bg-pink hover:text-black'
 							}
 						/>
 					</div>
-				</div>
-				<div>
-					<h2 className="capitalized mx-auto max-w-md pt-28 text-center font-poppins text-4xl font-[500] md:max-w-2xl md:text-6xl">
-						tu huella en la red
-					</h2>
-					<div className="my-10 mx-auto flex max-w-lg flex-col gap-5 text-center">
-						<div className="flex flex-col gap-8 font-sourceCode text-xl ">
-							<p>{copy.huellaFillerText1}</p>
-							<p>{copy.huellaFillerText2}</p>
-							<p>{copy.tagLine}</p>
-						</div>
-						<div></div>
+				</motion.div>
+				<motion.div
+					initial={{ opacity: 0, scale: 0.2 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{
+						duration: 1.5,
+						delay: 1,
+						type: 'spring',
+					}}
+					className="col-span-1 grid aspect-square w-[70%] place-self-center rounded-full border-[1px] border-orange bg-pink md:w-[50%]"
+				>
+					<Image src={logo} alt={'Logo de diazpugh'} />
+				</motion.div>
+			</article>
+			<div>
+				<h2 className="capitalized mx-auto max-w-md pt-28 text-center font-poppins text-4xl font-[500] md:max-w-2xl md:text-6xl">
+					tu huella en la red
+				</h2>
+				<div className="my-10 mx-auto flex max-w-lg flex-col gap-5 text-center">
+					<div className="flex flex-col gap-8 font-sourceCode text-xl text-black">
+						<p>{copy.huellaFillerText1}</p>
+						<p>{copy.huellaFillerText2}</p>
+						<p>{copy.tagLine}</p>
 					</div>
 				</div>
-			</article>
+			</div>
 
 			<article className="w-screen lg:h-screen">
 				<h2 className="capitalized mx-auto max-w-md pt-28 text-center font-poppins text-4xl font-[500] text-blue md:max-w-2xl md:text-6xl">
